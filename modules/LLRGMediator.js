a95caaf2f84b22b9cd08d44ad5feb9800ec0eef3
Carl Orthlieb <github@orthlieb.com>
2024-11-16 16:37:10 -0800

    Update radar graphs to use LLTable

--- modules/LLRGMediator.js
+++ modules/LLRGMediator.js
@@ -9,7 +9,7 @@ import { DEBUG } from "./Debug.js";
 import { STRINGS } from "./Strings.js";
 
 import { RadarChart } from "./RadarChart.js";
-import { DTTable } from "./DTTable.js";
+import { LLTable } from "./LLTable.js";
 import { LLPerson } from "./Person.js";
 
 /** @class */
@@ -30,14 +30,13 @@ export class LLRGMediator {
         if (companyName)
             $('.companyname').html(companyName).removeClass('d-none');
         
-        let tableData = this._prepTableData(this.people);
-        this.theTable = new DTTable(tableId, tableData, this);
+        this.theTable = new LLTable(tableId, this.people, this);
         
         // Columns holds the state of whether a particular dataset is visible or hidden.
         this.columnState = {};
         COMMON.llKeys.forEach(key => this.columnState[key] = true);
         let chartData = this._prepChartData(this.columnState, this.people);
-        this.theChart = new RadarChart(graphId, chartData, { displayLegend: true, legendPosition: 'top' }, this);
+        this.theChart = new RadarChart(graphId, chartData, { displayLegend: true, legendPosition: 'bottom' }, this);
     }
     
     /**
@@ -101,55 +100,6 @@ export class LLRGMediator {
     }
 
     /**
-     * Prepare the data for use in the table.
-     * @param {array} people An array of person objects.
-     * @returns {object} Prepared data for initializing and loading the table.
-     * @private
-     */
-    _prepTableData(people) {
-        DEBUG.logArgs('Mediator._prepTableData(people)', arguments);
-
-        let columns = COMMON.llKeys.map(key => { 
-            return { 
-                name: key, 
-                data: key, 
-                title: STRINGS.columnLabels[COMMON.llKeys.indexOf(key) + 2], 
-                orderSequence: ['desc', 'asc'] 
-            };
-        });
-        columns.unshift({ name: 'name', data: 'fullName', title: STRINGS.columnLabels[1] }); 
-        columns.unshift({ name: 'state', data: 'state', title: STRINGS.columnLabels[0] });
-        columns.push({ 
-            name: 'overallIntensity', 
-            data: 'overallIntensity', 
-            title: STRINGS.columnLabels[9], 
-            orderSequence: ['desc', 'asc']
-        });
-        let tableData = {
-            data: people,
-            columns: columns
-        };
-        
-        tableData.layout = {
-            topStart: null,
-            topEnd: {
-                buttons: [
-                    {
-                        text: STRINGS.general.columnVisibility,
-                        extend: 'colvis',
-                        columns: 'th:nth-child(n+3)',
-                        columnText: function (dt, nIndex, cTitle) {
-                            return STRINGS.columnLabels[nIndex];
-                        }
-                    }
-                ]
-            }
-        };
-        
-        return tableData;
-    }
-         
-    /**
      * Event listener when one of the rows in the table is unselected.
      * @param {array} aRows Array of row data that was selected.
      * @param {boolean} bSelect True if rows are to be selected, false otherwise.
@@ -172,44 +122,6 @@ export class LLRGMediator {
         }
     }
     
-    /**
-     * Update footer
-     * @method
-     * @param {array} data Array of the data in the table with each row containing an object with the key containing the datum.
-     * @param {array} selectedRows Array containing true/false as to which of the rows are selected.
-     * @param {array} visibleColumns Array containing true/false as to which of the columns are visible.
-     * @public
-     */
-    tableUpdateFooter(data, selectedRows, visibleColumns) {
-       DEBUG.log('Mediator.tableUpdateFooter(data, selectedRows, visibleColumns)', arguments);
-
-        // Find the average values for each column.
-        const aAverages = data.map(function (key, index) {
-            if (visibleColumns[index] && index > 1) {
-                // Data for selected rows
-                const aValues = selectedRows.map(row => { 
-                    return row[key];
-                });
-                return aValues.length > 0 ? (aValues.reduce((sum, val) => sum + val, 0) / aValues.length) : 0;
-            } else 
-                return undefined;   // Skip this column.
-        });
-        
-        // Build the footer
-        let arrows = [ 'bi-arrow-down', 'bi-arrow-down-right', 'bi-arrow-right', 'bi-arrow-up-right', 'bi-arrow-up' ];
-        let cFooter = aAverages.reduce((accumulator, nAverage) => {
-            if (nAverage == undefined)
-                return accumulator;
-            accumulator += '<th class="col-1 text-end">';
-            if (nAverage > 0)
-                accumulator += `<i class="bi ${arrows[COMMON.evaluateScoreLevel(nAverage)]} score-arrow"></i> ${Math.round(nAverage)}</th>`;
-            return accumulator += '</th>';
-        }, '<tr><th class="col-1"></th><th class="col-4">Group Average</th>');
-        cFooter += '</tr>';
-        
-        return cFooter;
-    }
-        
     /**
      * Event listener when an entry in the chart legend is clicked.
      * @method
--- modules/PRGMediator.js
+++ modules/PRGMediator.js
@@ -9,7 +9,7 @@ import { DEBUG } from "./Debug.js";
 import { STRINGS } from "./Strings.js";
 
 import { RadarChart } from "./RadarChart.js";
-import { DTTable } from "./DTTable.js";
+import { LLTable } from "./LLTable.js";
 import { LLPerson } from "./Person.js";
         
 /** @class */
@@ -31,14 +31,13 @@ export class PRGMediator {
         if (companyName)
             $('.companyname').html(companyName).removeClass('d-none');
         
-        let tableData = this._prepTableData(this.people);
-        this.theTable = new DTTable(tableId, tableData, this);
+        this.theTable = new LLTable(tableId, this.people, this);
         
         // Columns holds the state of whether a particular dataset is visible or hidden.
         this.columnState = {};
         COMMON.llKeys.forEach(key => this.columnState[key] = true);
         let chartData = this._prepChartData(this.columnState, this.people);
-        this.theChart = new RadarChart(graphId, chartData, { displayLegend: true, legendPosition: 'top' }, this);
+        this.theChart = new RadarChart(graphId, chartData, { displayLegend: true, legendPosition: 'bottom' }, this);
     }
     
     /**
@@ -104,60 +103,6 @@ export class PRGMediator {
     }
 
     /**
-     * Prepare the data for use in the table.
-     * @method
-     * @param {array} people An array of person objects.
-     * @returns {object} Prepared data for initializing and loading the table.
-     * @private
-     */
-    _prepTableData(people) {
-        DEBUG.log('Mediator._prepTableData(people)', arguments);
-
-        let columns = COMMON.llKeys.map(key => { 
-            return { 
-                name: key, 
-                data: key, 
-                title: STRINGS.labels[key][0], 
-                orderSequence: ['desc', 'asc'] 
-            };
-        });
-        columns.unshift({ name: 'name', data: 'fullName', title: STRINGS.general.fullName }); 
-        columns.unshift({ name: 'state', data: 'state', title: '' });
-        columns.push({ 
-            name: 'overallIntensity', 
-            data: 'overallIntensity', 
-            title: STRINGS.general.overallIntensityColumn, 
-            orderSequence: ['desc', 'asc']
-        });
-        let tableData = {
-            data: people,
-            columns: columns
-        };
-        
-        tableData.layout = {
-            topStart: null,
-            topEnd: {
-                buttons: [
-                    {
-                        text: STRINGS.general.columnVisibility,
-                        extend: 'colvis',
-                        columns: 'th:nth-child(n+3)',
-                        columnText: function (dt, nIndex, cTitle) {
-                            if (nIndex == 9)
-                                return STRINGS.general.overallIntensity;
-                            if (nIndex > 1)
-                                return STRINGS.labels[COMMON.llKeys[nIndex - 2]];
-                            return cTitle;
-                        }
-                    }
-                ]
-            }
-        };
-        
-        return tableData;    
-    }
-         
-    /**
      * Event listener when one of the rows in the table is unselected.
      * @method
      * @param {array} aRows Array of row data that was selected.
@@ -198,44 +143,6 @@ export class PRGMediator {
         }
     }
         
- /**
-     * Update footer
-     * @method
-     * @param {array} data Array of the data in the table with each row containing an object with the key containing the datum.
-     * @param {array} selectedRows Array containing true/false as to which of the rows are selected.
-     * @param {array} visibleColumns Array containing true/false as to which of the columns are visible.
-     * @public
-     */
-    tableUpdateFooter(data, selectedRows, visibleColumns) {
-       DEBUG.log('Mediator.tableUpdateFooter(data, selectedRows, visibleColumns)', arguments);
-
-        // Find the average values for each column.
-        const aAverages = data.map(function (key, index) {
-            if (visibleColumns[index] && index > 1) {
-                // Data for selected rows
-                const aValues = selectedRows.map(row => { 
-                    return row[key];
-                });
-                return aValues.length > 0 ? (aValues.reduce((sum, val) => sum + val, 0) / aValues.length) : 0;
-            } else 
-                return undefined;   // Skip this column.
-        });
-        
-        // Build the footer
-        let arrows = [ 'bi-arrow-down', 'bi-arrow-down-right', 'bi-arrow-right', 'bi-arrow-up-right', 'bi-arrow-up' ];
-        let cFooter = aAverages.reduce((accumulator, nAverage) => {
-            if (nAverage == undefined)
-                return accumulator;
-            accumulator += '<th class="col-1 text-end">';
-            if (nAverage > 0)
-                accumulator += `<i class="bi ${arrows[COMMON.evaluateScoreLevel(nAverage)]} score-arrow"></i> ${Math.round(nAverage)}</th>`;
-            return accumulator += '</th>';
-        }, '<tr><th class="col-1"></th><th class="col-4">Group Average</th>');
-        cFooter += '</tr>';
-        
-        return cFooter;
-    }
-    
     /**
      * Event listener when an entry in the chart legend is clicked.
      * @method
